f112ebbc61ebac93e88de5502fc2576e64443144
Alec Elton <alec.elton@gmail.com>
2012-12-28 21:02:36 -0600

    Correct case for YAML backend, also avoids conflicts with builtin yaml library

--- multiconfig/backends.py
+++ multiconfig/backends.py
@@ -1,7 +1,7 @@
 import yaml
 from config import MCBackend
 
-class yaml(MCBackend):
+class YAML(MCBackend):
 	def load(self, file):
 		with open(file, "r") as fp:
 			self.configuration.append(yaml.load(fp))
